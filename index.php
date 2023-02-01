f6b6f746487db95a29669b61cd4810940d9ec023
Kilian Olry <kilian.olry@gmail.com>
2023-02-01 11:48:47 +0100

    Mise en place du projet

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="asset/css/variables.css">
+    <link rel="stylesheet" href="asset/css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
+    <title>Blog</title>
+</head>
+
+<body>
+    <div class="container">
+        <?php require_once 'asset/includes/header.php' ?>
+        <div class="content">
+            <?php require_once 'asset/includes/footer.php' ?>
+        </div>
+    </div>
+</body>
+
+</html>
